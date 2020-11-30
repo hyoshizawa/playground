e45bdad04a723de78d3de9262b77d69f12a8d8a9
hyoshizawa <inzembla@gmail.com>
2020-11-30 23:34:51 +0900

    add binomial distribution.

--- ml_with_web_optimization/chapter_01.py
+++ ml_with_web_optimization/chapter_01.py
@@ -4,10 +4,10 @@ import numpy as np
 from matplotlib import pyplot as plt
 
 
-def main():
-    """
+def main_01():
     """
 
+    """
     thetas = np.linspace(0, 1, 1001)
     print(thetas)
 
@@ -42,3 +42,26 @@ def main():
     plt.xlabel(r'$\theta$')
     plt.ylabel(r'$p(\theta)$')
     plt.show()
+
+
+def main_02():
+    """
+    Binomial
+    """
+    thetas = np.linspace(0, 1, 1001)
+    print(thetas)
+
+    likelihood = lambda a, N: thetas ** a * (1 - thetas) ** (N - a)
+
+    def posterior(a, N, prior):
+        lp = likelihood(a, N) * prior
+        return lp / lp.sum()
+
+    prior = 1 / len(thetas)
+    plt.subplot(2, 1, 1)
+    plt.plot(thetas, posterior(2, 40, prior), label='Alice - A')
+    plt.plot(thetas, posterior(4, 50, prior), label='Alice - B')
+    plt.xlabel(r'$\theta$')
+    plt.ylabel(r'$p(\theta)$')
+    plt.xlim(0, 0.2)
+    plt.legend()
