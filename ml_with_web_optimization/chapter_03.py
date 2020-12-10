d4ec919746aa47d299ebeacef20a971716780a33
hyoshizawa <inzembla@gmail.com>
2020-12-10 09:13:09 +0900

    add ch03_03 function.

--- ml_with_web_optimization/chapter_03.py
+++ ml_with_web_optimization/chapter_03.py
@@ -49,3 +49,32 @@ def ch03_02():
         pm.plot_posterior(trace_comb, var_names=['beta'], hdi_prob=0.95)
 
         print((trace_comb['beta'][:, 1] > 0).mean())
+
+
+def ch03_03():
+    """ Comparing thetas by design.
+    """
+    n = [434, 382, 394, 88]  # page views.
+    clicks = [8, 17, 10, 4]
+    with pm.Model() as model:
+        theta = pm.Uniform('theta', lower=0, upper=1, shape=len(n))
+        obs = pm.Binomial('obs', p=theta, n=n, observed=clicks)
+        trace = pm.sample(5000, chains=2)
+
+    img = [0, 0, 1, 1]
+    btn = [0, 1, 0, 1]
+    with pm.Model() as model_comb:
+        alpha = pm.Normal('alpha', mu=0, sigma=10)
+        beta = pm.Normal('beta', mu=0, sigma=10, shape=2)
+        comb = alpha + beta[0] * img + beta[1] * btn
+        theta = pm.Deterministic('theta', 1 / (1 + pm.math.exp(-comb)))
+        obs = pm.Binomial('obs', p=theta, n=n, observed=clicks)
+        trace_comb = pm.sample(5000, chains=2)
+
+    with pm.Model():
+        pm.forestplot([trace, trace_comb], var_names=['theta'],
+                      hdi_prob=0.95, combined=True,
+                      model_names=['Individual', 'Combined'])
+
+    print((trace_comb['theta'][:, 1] - trace_comb['theta'][:, 0] > 0).mean())
+    print((trace_comb['theta'][:, 3] - trace_comb['theta'][:, 0] > 0).mean())
