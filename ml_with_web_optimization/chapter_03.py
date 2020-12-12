91c1d439a2137a6fa911b7911569530e3d2ab40b
hyoshizawa <inzembla@gmail.com>
2020-12-12 18:48:44 +0900

    finish chapter-03.

--- ml_with_web_optimization/chapter_03.py
+++ ml_with_web_optimization/chapter_03.py
@@ -6,6 +6,7 @@ Some notes:
 import numpy as np
 from matplotlib import pyplot as plt
 import pymc3 as pm
+from mpl_toolkits.mplot3d import Axes3D
 
 
 def ch03_01():
@@ -78,3 +79,63 @@ def ch03_03():
 
     print((trace_comb['theta'][:, 1] - trace_comb['theta'][:, 0] > 0).mean())
     print((trace_comb['theta'][:, 3] - trace_comb['theta'][:, 0] > 0).mean())
+
+
+def ch03_04():
+    """ Interaction term.
+    """
+    n = [434, 382, 394, 412]  # page views.
+    clicks = [8, 17, 10, 8]
+    img = [0, 0, 1, 1]
+    btn = [0, 1, 0, 1]
+
+    with pm.Model() as model_comb2:
+        alpha = pm.Normal('alpha', mu=0, sigma=10)
+        beta = pm.Normal('beta', mu=0, sigma=10, shape=2)
+        comb = alpha + beta[0] * img + beta[1] * btn
+        theta = pm.Deterministic('theta', 1 / (1 + pm.math.exp(-comb)))
+        obs = pm.Binomial('obs', p=theta, n=n, observed=clicks)
+        trace_comb2 = pm.sample(5000, chains=2)
+        pm.traceplot(trace_comb2, compact=True)
+
+    with pm.Model() as model_int:
+        alpha = pm.Normal('alpha', mu=0, sigma=10)
+        beta = pm.Normal('beta', mu=0, sigma=10, shape=2)
+        gamma = pm.Normal('gamma', mu=0, sigma=10)
+        comb = alpha + beta[0] * img + beta[1] * btn + gamma * img * btn
+        theta = pm.Deterministic('theta', 1 / (1 + pm.math.exp(-comb)))
+        obs = pm.Binomial('obs', p=theta, n=n, observed=clicks)
+        trace_int = pm.sample(5000, chains=2)
+        pm.traceplot(trace_int, compact=True)
+
+    x1 = np.arange(0, 1, 0.1)
+    x2 = np.arange(0, 1, 0.1)
+    X1, X2 = np.meshgrid(x1, x2)
+    fig = plt.figure()
+    ax = Axes3D(fig)
+    logit_theta = (trace_comb2['alpha'].mean() +
+        trace_comb2['beta'][:, 0].mean() * X1 +
+        trace_comb2['beta'][:, 1].mean() * X2)
+    surf = ax.plot_surface(X1, X2, logit_theta, cmap='plasma')
+    fig.colorbar(surf)
+    ax.set_xlabel(r'$x_1$')
+    ax.set_ylabel(r'$x_2$')
+    ax.set_zlabel(r'$logit(\theta)$')
+    plt.show()
+
+    # interaction.
+    x1 = np.arange(0, 1, 0.1)
+    x2 = np.arange(0, 1, 0.1)
+    X1, X2 = np.meshgrid(x1, x2)
+    fig = plt.figure()
+    ax = Axes3D(fig)
+    logit_theta = (trace_int['alpha'].mean() +
+                   trace_int['beta'][:, 0].mean() * X1 +
+                   trace_int['beta'][:, 1].mean() * X2 +
+                   trace_int['gamma'].mean() * X1 * X2)
+    surf = ax.plot_surface(X1, X2, logit_theta, cmap='plasma')
+    fig.colorbar(surf)
+    ax.set_xlabel(r'$x_1$')
+    ax.set_ylabel(r'$x_2$')
+    ax.set_zlabel(r'$logit(\theta)$')
+    plt.show()
