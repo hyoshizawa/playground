6029dd60f72d8b1adf94e9c2e490dc0f95eabbf4
hyoshizawa <inzembla@gmail.com>
2021-01-10 22:46:40 +0900

    add Annealing.

--- ml_with_web_optimization/chapter_05.py
+++ ml_with_web_optimization/chapter_05.py
@@ -34,7 +34,29 @@ class EpsilonGreedyAgent(object):
 
     def sample(self, arm, reward):
         self.counts[arm] += 1
-        self.values[arm] += (
+        self.values[arm] = (
+            (self.counts[arm] - 1) * self.values[arm] + reward
+        ) / self.counts[arm]
+
+
+class AnnealingEpsilonGreedyAgent(object):
+
+    def __init__(self, epsilon=1.0):
+        self.epsilon = epsilon
+        self.counts = np.zeros(n_arms)
+        self.values = np.zeros(n_arms)
+
+    def get_arm(self):
+        if np.random.random() < self.epsilon:
+            arm = np.random.randint(n_arms)
+        else:
+            arm = np.argmax(self.values)
+        self.epsilon *= 0.99
+        return arm
+
+    def sample(self, arm, reward):
+        self.counts[arm] += 1
+        self.values[arm] = (
             (self.counts[arm] - 1) * self.values[arm] + reward
         ) / self.counts[arm]
 
@@ -62,4 +84,18 @@ def ch05_01():
     plt.plot(acc)
     plt.xlabel(r'$t$')
     plt.ylabel(r'$\mathbb{E}[x(t) = x^*]$')
+    plt.show()
+
+
+def ch05_02():
+    """ """
+    arms_eg, rewards_eg = sim(EpsilonGreedyAgent)
+    arms_aeg, rewards_eg = sim(AnnealingEpsilonGreedyAgent)
+    plt.plot(np.mean(arms_aeg == Env.opt(), axis=0),
+             label=r'Annealing $\varepsilon$-greedy')
+    plt.plot(np.mean(arms_eg == Env.opt(), axis=0),
+             label=r'$\varepsilon$-greedy')
+    plt.xlabel(r'$t$')
+    plt.ylabel(r'$¥mathbb{E}[x(t) = x^*$')
+    plt.legend()
     plt.show()
