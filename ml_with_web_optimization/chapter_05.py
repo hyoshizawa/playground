b8c995c8ac86907f245b8c9ed416b03ebb044686
hyoshizawa <inzembla@gmail.com>
2021-01-17 22:30:11 +0900

    add AnnealingSoftmaxAgent.

--- ml_with_web_optimization/chapter_05.py
+++ ml_with_web_optimization/chapter_05.py
@@ -61,6 +61,72 @@ class AnnealingEpsilonGreedyAgent(object):
         ) / self.counts[arm]
 
 
+class SoftmaxAgent(object):
+
+    def __init__(self, tau=.05):
+        self.tau = tau
+        self.counts = np.zeros(n_arms)
+        self.values = np.zeros(n_arms)
+
+    def softmax_p(self):
+        logit = self.values / self.tau
+        logit = logit - np.max(logit)
+        p = np.exp(logit) / sum(np.exp(logit))
+        return p
+
+    def get_arm(self):
+        arm = np.random.choice(n_arms, p=self.softmax_p())
+        return arm
+
+    def sample(self, arm, reward):
+        self.counts[arm] = self.counts[arm] + 1
+        self.values[arm] = (
+            (self.counts[arm] - 1) * self.values[arm] + reward
+        ) / self.counts[arm]
+
+
+class AnnealingSoftmaxAgent(object):
+
+    def __init__(self, tau=1000.):
+        self.tau = tau
+        self.counts = np.zeros(n_arms)
+        self.values = np.zeros(n_arms)
+
+    def softmax_p(self):
+        logit = self.values / self.tau
+        logit = logit - np.max(logit)
+        p = np.exp(logit) / sum(np.exp(logit))
+        return p
+
+    def get_arm(self):
+        arm = np.random.choice(n_arms, p=self.softmax_p())
+        self.tau = self.tau * 0.9
+        return arm
+
+    def sample(self, arm, reward):
+        self.counts[arm] = self.counts[arm] + 1
+        self.values[arm] = (
+            (self.counts[arm] - 1) * self.values[arm] + reward
+        ) / self.counts[arm]
+
+
+class BernoulliTSAgent(object):
+
+    def __init__(self):
+        self.counts = [0 for _ in range(n_arms)]
+        self.wins = [0 for _ in range(n_arms)]
+
+    def get_arms(self):
+        beta = lambda N, a: np.random.beta(a + 1, N - a + 1)
+        result = [beta(self.counts[i], self.wins[i]) for i in range(n_arms)]
+        arm = result.index(max(result))
+        return arm
+
+    def sample(self, arm, reward):
+        self.counts[arm] = self.counts[arm] + 1
+        self.wins[arm] = self.wins[arm] + reward
+
+
 def sim(Agent, N=1000, T=1000, **kwargs):
     selected_arms = [[0 for _ in range(T)] for _ in range(N)]
     earned_rewards = [[0 for _ in range(T)] for _ in range(N)]
@@ -97,5 +163,19 @@ def ch05_02():
              label=r'$\varepsilon$-greedy')
     plt.xlabel(r'$t$')
     plt.ylabel(r'$¥mathbb{E}[x(t) = x^*$')
+    plt.legend()
+    plt.show()
+
+
+def ch05_03():
+    """"""
+    arms_sm, rewards_sm = sim(SoftmaxAgent)
+    arms_asm, rewards_asm = sim(AnnealingSoftmaxAgent)
+
+    plt.plot(np.mean(arms_asm == Env.opt(), axis=0),
+             label='Annealing Softmax')
+    plt.plot(np.mean(arms_sm == Env.opt(), axis=0), label='softmax')
+    plt.xlabel(r'$t$')
+    plt.ylabel(r'$\mathbb{E}[x(t) = x^*]$')
     plt.legend()
     plt.show()
